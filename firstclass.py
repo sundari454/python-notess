7b71be22cb664608d750e205d269de22c6f699b5
sundari gupta <ruhi99641@gmail.com>
2024-06-20 13:06:29 +0530

    Add files via upload

---
+++ firstclass.py
@@ -0,0 +1,463 @@
+
+
+"""print("hello world!")
+
+print("welcome to the villa🫠🫠")
+
+
+
+
+x="india"
+print(x)
+
+x=1500
+y=2000
+print(x,y)
+A= "HELLO WORLD"
+print(A)
+A_2="pakistaan🪷"
+print(A_2)
+print(A)
+print("addition :-",x+y)
+print("divide:- ",x/y)
+print("modulas:- ",x*y)
+print("substract:-",x-y)
+x = 45
+y = 60
+z = 80
+print("addition:-",x+y+z)
+a,b,c,d=5,4,8,9
+R = "RUHI"
+print("modulas:-",a*b*c*d)
+print("name =",R)
+
+A=23
+B=23.23
+C=23j
+D="RUHI"
+E=["data","science",343,45.45]
+F=("data","science",343,45.45)
+G= range(2,50,5)
+H={"data","science",343,45.45}
+print(A)
+print(type(A))
+print(B)
+print(type(B))
+print(C)
+print(type(C))
+print(D)
+print(type(D))
+print(E)
+print(type(E))
+print(F)
+print(type(F))
+print(G)
+print(type(G))
+print(H)
+print(type(H))
+Z="data science"
+print(Z)
+print(len(Z)-1)
+x=10
+y=20
+print(x+y)
+Q="10"
+R="20"
+print(Q+R)
+S="data "
+T="science"
+print(S+T)
+O=456
+print(type(O))
+O=str(O)
+print(type(O))
+print(O*5)"""
+
+
+"""N="125"
+R=float(N)
+print(R)
+print(type(R))
+M=complex(N)
+print(M)
+print(type(M))
+A1=list(N)
+print(A1)
+print(type(A1))
+A12=int(N)
+print(A12)
+print(type(A12))"""
+
+
+
+
+'''print(12/12)
+
+X=120.789
+y=str(X)
+print(len(y))
+
+'''
+
+
+
+'''x=45
+y=20
+print("modules :-",x%y)
+print("The reminder of",x,"and",y,"is:-",x%y)
+
+X=5
+Y=3
+print(X*Y)
+print("exponentation of 5 is",X**Y)
+
+print(50+10*0+7+2)
+A=19
+B=5
+print(A//B)
+print(45//8.5)
+print(2**4)
+
+Q=45
+Q2=45
+print(Q==Q2)
+print(Q!=Q2)
+
+r=50
+r=r+5
+print(r)
+r+=5
+print(r)
+r-=5
+print(r)
+r*=5
+print(r)
+r/=5
+print(r)'''
+
+## 13-05-2024
+
+
+# write a python program to show the number if number is greater
+# than 10 than print  yes else print no.
+'''x= 15
+''if x > 10:
+	print("yes")
+else:
+	print("no")
+
+y= 45
+if y> 50:
+	print("yes it is grater than 50")
+else:
+	print("it is not greater than 50")
+
+z= 55
+if z> 50:
+	print("yes it is grater than 50")
+else:
+	print("it is not greater than 50")
+
+# write a python programm to check the number is odd or even
+	
+a=15
+if a%2 == 0:
+	print("even number")
+else:
+	print("odd")
+
+#write a python programm to checknumber is odd
+x=19
+if x%2 == 1:
+	print("odd")
+
+#check the number is divisible 
+#of 9 or not
+81,75,40
+v=81
+if v%9 ==0:
+	print("divisible")
+
+n=75
+if n%9 ==0:
+	print("divisible")
+
+
+t=40
+if t%9 ==0:
+	print("divisible")'''
+
+#user input:-ii
+
+#if c%2 == 0:
+#	print("even number")
+#else:
+	 #print("odd")
+
+#write a python programm with the help of user input
+# and show the number is greater than 200 then print
+# yes grater than 200 else print less than 200
+
+
+"""x=int(input("Enter any number:-"))
+if x>200:
+	print("yes grater than 200")
+else:
+	print("less than 200")"""
+
+#check the text is equal to delhi then print yes else no
+"""y=str(input("Enter the value:-"))
+if y== "delhi":
+	print("yes")
+else:
+	print("no")"""
+
+
+#write a python program to show the state and capital 
+# accordingto user input
+
+'''y=str(input("Enter the value:-"))
+if y== "Bihar":
+	print("patna")
+else:
+	print("not available")'''
+
+'''x=15
+y=10
+print(x>10 and y>9)
+
+if x>10 and y>9:
+	print("Both condition are true")
+else:
+	print("wrong condition")
+
+a=25
+
+if a>20 and a<30:
+	print("yes")
+else:
+	print("no")'''
+
+
+
+
+# y=int(input("Enter the value:-"))
+# if y%10== 0 or y%5== 0:
+# 	print("yes")
+# else:
+# 	print("not available")
+
+
+#14-05-2024
+#write a python pragram to check and comapare two
+#who is greater number.
+'''x=45
+y=13
+
+if x>y:
+	print("greater than value")
+else:
+	print("other")'''
+
+#WRITE A PYTHON PROGRAM TO CHECK THE NUMBER IS DIVISIBLE
+#OF 5 AND 10
+'''A=int(input("Enter number:-"))
+if A%5==0 and A%10==0:
+	print("divisible of 5 and 10")
+else:
+	print("not divisible")'''
+
+
+#WITE A PYTHON PROGRAM TO PRINT THE LAST DIGIT OF ANY NUMBER
+#if number IS ODD 
+'''Q=int(input("enter the number="))
+if Q%2==1:
+	print(Q%10)
+else:
+	print("other")'''
+
+
+#WRITE A PYTHON PROGRAMWITH THE HELP OF USER INPUT TO CHECK
+#IF PASSWORD IS EQUAL THE PRINT "CORRECT PASSWORD" ELSE
+#PRINT "WRONG PASSWORD".
+'''e=str(input("enter the value:-"))
+if e=="equal":
+	print("correct password")
+else:
+	print("wrong password")
+
+	##### or
+pas=1234
+pin=int(input("enter the password:-"))
+if pin==pas:
+	print("correct password")
+else:
+	print("wrong password")
+'''
+# write a python to print the discount and discounted amount 
+#if  amount is less than 5000 than 20% else 30%
+
+'''r=float(input("amount:-"))
+if r>5000:
+	print(r-r*0.2)
+else:
+	print(r-r*0.3)
+	#or
+amount =float(input("enter the amunt :-"))
+if amount<5000:
+	d=amount*0.2
+	a=amount-d
+else:
+	d=amount*0.3
+	a=amount-d
+print("Discount:-",d)
+print("After discount:-",a)'''
+
+'''x=int(input("enter the value:-"))
+if x>=1 and x<10:
+	print("1st")
+elif x>=10 and x<20:
+	print("2nd")
+elif x>=20 and x<30:
+	print("3rd")
+else:
+	print("4th")
+
+      #or 
+x=int(input("enter the value:-"))
+if x>=1 and x<10:
+	a="1st"
+elif x>=10 and x<20:
+	a="2nd"
+elif x>=20 and x<30:
+	a="3rd"
+else:
+	a="4th"
+print(a)'''
+
+'''q=float(input("enter the value:-"))
+if  q>=60:
+	print("1st")
+elif q>=45:
+	print("2nd")
+elif q>=33:
+	print("3rd")
+else:
+	print("fail")
+'''
+
+'''x=int(input("enter the number:-"))
+y=int(input("enter the number:-"))
+z=int(input("enter the number:-"))
+if y<x>z:
+	a=x,"is greatest"
+elif x<y>z:
+	a=y,"is greatest"
+else:
+	a=z,"is greater"
+print(a)
+
+'''
+## 15-05-2024
+'''x=45
+y=10
+print("x:-",x)
+print("y:-",y)
+print("after the update")
+x,y=y,x
+
+print("x:-",x)
+print("y:-",y)
+
+a=20
+w=30
+q=40
+a,w,q=q,w,a
+print(a)
+print(w)
+print(q)
+'''
+'''x=100
+y=200
+z=300
+x,z=z,x
+y,z=z,y
+
+print(x)
+print(y)
+print(z)'''
+
+'''a=10
+b=20
+c=30
+d=40
+'''
+
+'''x,y="59"
+z=12
+x,y=z,x
+z,x=z,y
+x=y
+print(x)
+print(y)
+print(z)'''
+
+#member ship operator
+'''x=[23,4,5,6,7,8,10]
+print(24 in x)
+print(10 in x'''
+	#write a pythion program with the help of user input to print
+"""x=str(input("enter the value:-"))
+if "a" in x or "i" in x:
+	print("yes")
+else:
+	print("no")"""
+'''
+y=["sunday","monday","tuesday","wednessday"]
+if "sunday" in y:
+	print("present")
+else:
+	print("not present")'''
+
+
+
+# write a python programm to show the grade if number is greater
+# than 90 then print"a+"
+# if number is greater than 90 then print"a+"
+# if number is greater than 80 then print"a"
+# if number is greater than 70 then print"b+"
+# if number is greater than 60 then print"b"
+# if number is greater than 50 then print"C"
+'''# else print "d"
+x=int(input("enter the number:-"))
+if x>90:
+	g="A+"
+elif x>80:
+	g="A"
+elif x>70:
+	g="B+"
+elif x>60:
+	g="B"
+elif x>50:
+	g="C"
+else:
+	g="D"
+print(g)
+
+'''
+
+
+# write a query to show year and leap year 
+# with the help
+#  of user input
+'''x=int(input("enter the year:-"))
+if x%4 == 0:
+	print("leapyear")
+else:
+	print("not leapyear")'''
+
+'''x="fr32"
+a=len(x)
+print("Length of X:-",a)
+b=type(x)
+print("data type of X:-",b)'''
